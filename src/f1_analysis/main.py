8e3ba2b1be8272c5c55e38b8897df9578d60a173
admin <admin@admins-MacBook-Air.local>
2026-08-16 17:00:45 +0100

    Added lap feature for comparing two drivers laps throughout a race

--- src/f1_analysis/main.py
+++ src/f1_analysis/main.py
@@ -16,7 +16,14 @@ session = load_session(2024, "Silverstone", "R")
 results = session.results
 laps = session.laps
 
+d1, d2 = "Lewis Hamilton", "Max Verstappen"
 
-#print(result_features.display_classification(results))
-driver = input("enter a driver: ").title()
-print(lap_analysis.display_every_lap(laps, driver))
+lap_comparisons = lap_analysis.comapare_drivers_laps(laps, d1, d2)
+count = 0
+for lap in lap_comparisons:
+    if lap < 0:
+        print(f"lap {count}: {d1} was {lap} faster than {d2}")
+    else:
+        print(f"lap {count}: {d1} was +{lap} slower than {d2}")
+    count+=1
+
